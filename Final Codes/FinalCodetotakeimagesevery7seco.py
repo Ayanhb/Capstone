db082e3bd2f4b5f7377b14ac64f8d961a7633a0b
Ayanhb <135760082+Ayanhb@users.noreply.github.com>
2024-04-07 20:05:24 -0700

    Update FinalCodetotakeimagesevery7seco.py

--- Final Codes/FinalCodetotakeimagesevery7seco.py
+++ Final Codes/FinalCodetotakeimagesevery7seco.py
@@ -22,6 +22,7 @@ picam2.stop_preview()
 
 
 import time
+import numpy as np
 from datetime import datetime
 from picamera2 import Picamera2, Preview
 
@@ -33,6 +34,8 @@ def capture():
         picam2.capture_file('//home/ayan/Pictures/JPG/%s.jpg' % timestamp)
         with picam2.capture_stream('dng') as stream:
             picam2.capture(stream, 'dng')
+            array = np.frombuffer(stream.getvalue(), dtype=np.uint16)
+            array = array.reshape((4056, 3040))
             with open('//home/ayan/Pictures/DNG/%s.dng' % timestamp, 'wb') as f:
                 f.write(stream.getvalue())
         time.sleep(7)
