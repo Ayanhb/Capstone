af2064f52235c0fdfcf6cfc3aeb09480e3285764
Ayanhb <135760082+Ayanhb@users.noreply.github.com>
2024-04-27 21:45:34 -0700

    Update FCI.py
    Final Code for the Imaging System that includes the logging file

--- Final Codes/FinalCodetotakeimagesevery7seco.py
+++ Final Codes/FinalCodetotakeimagesevery7seco.py
@@ -1,37 +1,25 @@
-# Code to take images every 7 seconds just JPG 
-import time
+#Final Code for Image System on Flight 
+import time 
+import os 
+import logging 
 from datetime import datetime
-from picamera2 import Picamera2, Preview
+from logging.handlers import TimedRotatingFileHandler
+from picamera2 import Picamera2, Preview 
 
-picam2 = Picamera2()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s\t%(levelname)s\t%(message)s')
+logname = "/home/ayan/Pictures/Log/Telemetry.log"
+handler = TimedRotatingFileHandler(logname, when="m", interval=10)
+handler.setLevel(logging.INFO)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
-def capture():
-    while True:
-        timestamp = datetime.now().isoformat()
-        picam2.capture_file('//home/ayan/Pictures/JPG/%s.jpg' % timestamp)
-        time.sleep(7)
+os.environ['LIBCAMERA_LOG_LEVELS'] = '4'
+Picamera2.set_logging(Picamera2.ERROR)
+picam2 - Picamera2()
 
-camera_config = picam2.create_still_configuration(main={"size": (4056, 3040)}, lores={"size": (4056, 3040)}, display="lores")
-picam2.configure(camera_config)
-picam2.start_preview(Preview.QTGL)
-picam2.start()
-
-capture()
-
-picam2.stop_preview()
-
-# Combined JPG AND DNG SO THIS CODE
-# Final Code to take images every 7 seconds in both JPG and DNG formats
-import time
-from datetime import datetime
-from picamera2 import Picamera2, Preview
-
-picam2 = Picamera2()
-picam2.start_preview(Preview.QTGL)
-
-preview_config = picam2.create_preview_configuration()
 capture_config = picam2.create_still_configuration(main={"size": (4056, 3040)}, lores={"size": (4056, 3040)}, display="lores", raw={})
-picam2.configure(preview_config)
 
 picam2.start()
 
@@ -43,5 +31,3 @@ def capture():
         time.sleep(4)
 
 capture()
-
-picam2.stop_preview()
